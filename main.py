5892e50e2fa64aa62c27240486f125adaad1e229
Kiran <kiransettyks@gmail.com>
2022-01-31 19:02:26 +0200

    Connected Spotify to program Added authentification info as environment variables Accessed Spotify account and made Spotify for Developers app

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
+from pprint import pprint
+
 import requests
 import datetime
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
 
-site = requests.get(url="https://www.billboard.com/charts/hot-100/2000-08-12/")
+CLIENTID = os.environ.get('SPOTIPY_CLIENT_ID')
+SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
+URI = os.environ.get('SPOTIPY_REDIRECT_URI')
+
 date_now = datetime.date.today()
 oldest_date = datetime.date(1958, 8, 4)
 year_now = date_now.year
-month_now = date_now.month
-day_now = date_now.day
-print(year_now)
-print(month_now)
-print(day_now)
 
-soup = BeautifulSoup(site.text, "html.parser")
+print(os)
 
-raw_titles = soup.find_all(id="title-of-a-story", name="h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
-titles = [soup.find(id="title-of-a-story", name="h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 u-font-size-23@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-245 u-max-width-230@tablet-only u-letter-spacing-0028@tablet").get_text()[1:-1]]
-for i in raw_titles:
-    titles.append(i.get_text()[1:-1])
-
-#Get artists
 
 def get_date():
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -68,4 +65,31 @@ while not date_now >= chosen_date >= oldest_date:
     print("The date you selected was not valid. Please choose a date between 4 August 1958 and today.")
     print(chosen_date)
     chosen_date = get_date()
+site = requests.get(
+    url=f"https://www.billboard.com/charts/hot-100/{chosen_date.year:04d}-{chosen_date.month:02d}-{chosen_date.day:02d}")
+soup = BeautifulSoup(site.text, "html.parser")
+raw_titles = soup.find_all(id="title-of-a-story", name="h3", class_="c-title a-no-trucate a-font-primary-bold-s "
+                                                                    "u-letter-spacing-0021 lrv-u-font-size-18@tablet "
+                                                                    "lrv-u-font-size-16 u-line-height-125 "
+                                                                    "u-line-height-normal@mobile-max "
+                                                                    "a-truncate-ellipsis u-max-width-330 "
+                                                                    "u-max-width-230@tablet-only")
+titles = [soup.find(id="title-of-a-story", name="h3", class_="c-title a-no-trucate a-font-primary-bold-s "
+                                                             "u-letter-spacing-0021 u-font-size-23@tablet "
+                                                             "lrv-u-font-size-16 u-line-height-125 "
+                                                             "u-line-height-normal@mobile-max a-truncate-ellipsis "
+                                                             "u-max-width-245 u-max-width-230@tablet-only "
+                                                             "u-letter-spacing-0028@tablet").get_text()[1:-1]]
+for i in raw_titles:
+    titles.append(i.get_text()[1:-1])
 
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENTID, client_secret=SECRET, redirect_uri=URI))
+
+results = sp.search(type='track', q=titles[1])
+pprint(results["tracks"]["items"][0]["name"])
+print(titles[1])
+tracks=[]
+#sp.user_playlist_add_tracks(user='jxaweaogwz7q2todcaaaphcky', playlist_id='', tracks=tracks, position=None)
+#sp.user_playlist_create(user='jxaweaogwz7q2todcaaaphcky', name=f'Hits from {chosen_date.year:04d}-{chosen_date.month:02d}-{chosen_date.day:02d}', public=True, description=f'The top 100 songs on the billboard on {chosen_date.year:04d}-{chosen_date.month:02d}-{chosen_date.day:02d}')
